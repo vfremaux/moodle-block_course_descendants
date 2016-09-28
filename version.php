61c4749bf1918786460e3d52440e3da73d7f7a9a
valery <valery.fremaux@gmail.com>
2016-09-28 11:15:43 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -17,20 +17,19 @@
 /**
  * Version details
  *
- * @package     block_course_descendants
- * @category    blocks
- * @author      Valery Fremaux (valery.fremaux@gmail.com)
- * @copyright   2O13 onwards Valery Fremaux (valery.fremaux@gmail.com)
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    block
+ * @subpackage course_descendants
+ * @copyright  2O13 Valery Fremaux (valery.fremaux@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013011800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015111100;        // Requires this Moodle version.
-$plugin->release = '3.0.0 (Build 2013011800)';
-$plugin->component = 'block_course_descendants'; // Full name of the plugin (used for diagnostics).
+$plugin->version   = 2013011800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2016051900;        // Requires this Moodle version
+$plugin->release = '3.1.0 (Build 2013011800)';
+$plugin->component = 'block_course_descendants'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.0.0000';
+$plugin->codeincrement = '3.1.0000';
