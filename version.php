c48ef56000c486fb5167b4dcc9b506f5b49b77fc
Valery Fremaux <valery.fremaux@gmail.com>
2025-04-28 12:12:17 +0200

    %1

--- version.php
+++ version.php
@@ -18,7 +18,6 @@
  * Version details.
  *
  * @package     block_course_descendants
- * @category    blocks
  * @author      Valery Fremaux <valery.fremaux@gmail.com>
  * @copyright   2012 onwards Valery Fremaux (http://www.mylearningfactory.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -26,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015062405;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025011400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041900;        // Requires this Moodle version.
-$plugin->release = '4.1.0 (Build 2015062405)';
-$plugin->supported = [401, 402];
+$plugin->release = '4.5.0 (Build 2015062405)';
+$plugin->supported = [401, 405];
 $plugin->component = 'block_course_descendants'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '4.1.0000';
+$plugin->codeincrement = '4.5.0000';
